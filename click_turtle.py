25b5cddc1f6b7f8349247c3c6c5395456453da72
nhammen-kinkaid <nathaniel.hammen@kinkaid.org>
2026-06-12 11:10:03 -0500

    Remove lines from the template so they can be shown in class

--- click_turtle.py
+++ click_turtle.py
@@ -6,18 +6,6 @@ window = turtle.Screen()
 
 # Create a visible turtle object
 my_turtle = turtle.Turtle()
-my_turtle.shape("turtle")
-my_turtle.shapesize(4)  # Make it larger and easier to click
-
-colors = ["red", "blue", "green", "purple", "orange"]
-my_turtle.color(random.choice(colors))
-
-# Define the click handler function (Must accept x, y)
-def handle_click(x, y):
-    my_turtle.right(90)
-
-# Bind the function to the turtle object click event
-my_turtle.onclick(handle_click)
 
 # Keep the window open and listening for events
 window.mainloop()
